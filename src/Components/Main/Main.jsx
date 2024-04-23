be62caf62106724cf3c84fe8018f9930801b4d7f
RabeyaAkter78 <rabeyariya251@gmail.com>
2024-04-23 12:21:02 +0600

    added footer section

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../../Pages/Shared/Navbar/NavBar";
 import SecondNav from "../../Pages/Shared/SecondNav/SecondNav";
+import ThirdNav from "../../Pages/Shared/TrirdNav/ThirdNav";
+import FirstFooter from "../../Pages/Shared/Footer/FirstFooter/FirstFooter";
 
 const Main = () => {
     return (
         <div>
             <NavBar></NavBar>
             <SecondNav></SecondNav>
+            <ThirdNav></ThirdNav>
             <Outlet></Outlet>
+            <FirstFooter></FirstFooter>
         </div>
     );
 };
---
+++ src/Pages/Shared/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,10 @@
+
+const SectionTitle = ({ heading }) => {
+    return (
+        <div className="font-bold mt-18 ml-2">
+            <h3 className="text-3xl text-black py-4">{heading}</h3>
+        </div>
+    );
+};
+
+export default SectionTitle;
