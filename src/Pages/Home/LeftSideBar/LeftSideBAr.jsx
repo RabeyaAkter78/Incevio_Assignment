371179ccba35add030dddd6d488fbdd7511d4fe7
RabeyaAkter78 <rabeyariya251@gmail.com>
2024-04-24 14:55:10 +0600

    modify responsiveness

--- src/Pages/Home/LeftSideBar/LeftSideBAr.jsx
+++ src/Pages/Home/LeftSideBar/LeftSideBAr.jsx
@@ -3,55 +3,55 @@ const LeftSideBar = () => {
     return (
         <div className='w-full md:w-3/12 md:px-16'>
             <div className="w-full md:w-64 bg-white ">
-                <div className="flex justify-center items-center gap-2 pb-4 ">
+                <div className="flex justify-center items-center pt-4 gap-2 pb-4 ">
                     <IoIosRibbon />
                     <h2 className='mb-2 font-bold text-[#194E8A]'>All Categories</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2 ">
+                <div className="flex gap-2 ">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Cars and Motorcycles</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex gap-2">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Music and Books</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex gap-2">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Arts and Crafts</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex gap-2">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Moms and Babies</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex gap-2">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Softwares</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex gap-2">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Home & Garden</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex gap-2">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Electronics</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex gap-2">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Kids and Toy</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex gap-2">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Clothing and Shoes</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex gap-2">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Beauty and Health</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex gap-2">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Sports</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2">
+                <div className="flex gap-2">
                     <IoIosRibbon />
                     <h2 className='mb-2 '>Jewellery</h2>
                 </div>
